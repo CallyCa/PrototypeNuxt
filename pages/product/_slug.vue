74b49a3f35b464d2ee7b25fb332d9fd8b819016b
callyca <blackyrite@hotmail.com>
2021-06-03 14:49:13 -0300

    Add: slug page product

--- pages/product/_slug.vue
+++ pages/product/_slug.vue
@@ -1,11 +1,42 @@
 <template>
-	<p>Texto</p>
+	<div>
+		<p>{{ item }}</p>
+	</div>
 </template>
 
 <script>
+import { api } from '@/services/index'
+// import axios from 'axios'
+// import FinishTransation from '@/componenets/FinishTransation/index'
+// import FinishTransation from '@/componenets/Product/ListProduct'
+
 export default {
 	name: 'Slug',
-	components: {},
+	components: {
+		// FinishTransation,
+	},
+	props: {
+		id: {
+			type: String,
+			default: '',
+		},
+	},
+	data() {
+		return {
+			sell: false,
+			item: null,
+		}
+	},
+	created() {
+		this.getItem()
+	},
+	methods: {
+		getItem() {
+			api.get(`/product/${this.id}`).then((response) => {
+				this.item = response.data
+			})
+		},
+	},
 }
 </script>
 
